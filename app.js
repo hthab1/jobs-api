84db37d0b056671f893ab40f4c60d6eee1f78d3e
Habteweld Yimer <ht@Habtewelds-MacBook-Pro.local>
2023-08-22 00:51:28 +0300

    swagger added

--- app.js
+++ app.js
@@ -14,6 +14,11 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+// Swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 const app = express();
 
 app.set("trust proxy", 1);
@@ -29,9 +34,15 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+// routes
+app.get("/", (req, res) => {
+  res.send("<h1>Jobs API</h1><a href='/api-docs'>Documentation</a>");
+});
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
-
 app.use(errorHandlerMiddleware);
 app.use(notFound);
 
